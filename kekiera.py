e2835559aec06abd3416ce8a50a07789f029b409
Brandon Leonardo <124607654+PlataYazul@users.noreply.github.com>
2023-03-01 12:10:59 -0500

    Update kekiera.py

--- kekiera.py
+++ kekiera.py
@@ -8,8 +8,24 @@ Se tomara como ejemplo el siguiente código:
 if __name__ == "__main__" :
   print("Hola mundo")
   
-  Primero en el bloc de notas como se muestra en la siguiente captura:
+  Primero en el bloc de notas como se muestra en la siguiente captura consultable en el enlace adjunto:
+    https://i.postimg.cc/nc7c0tT3/Captura-de-pantalla-1.png
+      
+  En la terminal del sistema luego de guardar el archivo de bloc de notas con la siguiente estructura de nombre "ejemplo.py"
   
+  https://i.postimg.cc/6qSsXT0D/Captura-de-pantalla-5.png
+    
+   Ahora el codigo inicial se ejecuta directamente en el interprete python:
+    
+    https://i.postimg.cc/gjLS3sVT/Captura-de-pantalla-3.png
+      
+   Finalmente, en la herramienta VS CODE.
+  
+  
+  https://i.postimg.cc/JhdSJswt/Captura-de-pantalla-2.png
+    
+    
+    Gracias por la atención. 
   
   
   
