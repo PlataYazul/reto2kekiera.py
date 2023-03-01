3f62b90e47b72ce01218a717878cf63fc6999bad
Brandon Leonardo <124607654+PlataYazul@users.noreply.github.com>
2023-03-01 11:43:12 -0500

    Create kekiera.py

---
+++ kekiera.py
@@ -0,0 +1,13 @@
+Las siguientes son herramientas utiles con las que es posible ejecutar y/o escribir código.
+- Bloc de notas
+- Python 3.1o
+- VS code
+
+
+Se tomara como ejemplo el siguiente código:
+if __name__ == "__main__" :
+  print("Hola mundo")
+  
+  Primero en el bloc de notas como se muestra en la siguiente captura:
+  
+  
